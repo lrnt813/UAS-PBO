fc90d6f429f089fa1129d8fde10349dfb71ac987
lrnt813 <137227773+lrnt813@users.noreply.github.com>
2024-06-26 07:56:48 +0700

    Add files via upload

---
+++ src/Pallete/RoundedTextField.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pallete;
+
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JTextField;
+
+public class RoundedTextField extends JTextField {
+
+    private Shape shape;
+    private final int arcWidth;
+    private final int arcHeight;
+
+    public RoundedTextField(int columns, int arcWidth, int arcHeight) {
+        super(columns);
+        this.arcWidth = arcWidth;
+        this.arcHeight = arcHeight;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fill(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, arcWidth, arcHeight));
+        super.paintComponent(g);
+        g2.dispose();
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getForeground());
+        g2.draw(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, arcWidth, arcHeight));
+        g2.dispose();
+    }
+
+    @Override
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, arcWidth, arcHeight);
+        }
+        return shape.contains(x, y);
+    }
+
+    @Override
+    public Insets getInsets() {
+        int value = Math.max(arcWidth, arcHeight) / 2;
+        return new Insets(value, value, value, value);
+    }
+}
